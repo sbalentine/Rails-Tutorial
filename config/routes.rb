7cfab4eba7eecf96d32b001d3e3f906a64a78778
Scott Balentine <sbalentine@yammer-inc.com>
2011-12-22 11:01:39 -0800

    Finished first half of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
   get "pages/home"
   get "pages/contact"
   get "pages/about"
   get "pages/help"
+  
+  resources :users
   
   match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
